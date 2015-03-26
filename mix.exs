465bdd203d0a009ca79324c8698a8f0740d5537e
hqmq <michael@riesd.com>
2015-03-25 22:23:09 -0600

    bump 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChunkySVG.Mixfile do
   def project do
     [
       app: :chunky_svg,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.0",
       deps: deps,
       package: package,
