6172ff8aee3d7d3789b7a2c965e893bf0caabb04
Michael Ries <riesmmm@gmail.com>
2021-12-14 12:49:42 +0000

    publish 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChunkySVG.Mixfile do
   def project do
     [
       app: :chunky_svg,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       deps: deps(),
       package: package(),
